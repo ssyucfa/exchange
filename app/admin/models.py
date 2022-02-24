0d58e854889e14d4a9bd1cee2a416e512c485f0c
ssyucfa <patin123321@bk.ru>
2022-02-24 17:59:18 +0300

    first commit

---
+++ app/admin/models.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from hashlib import sha256
+from typing import Optional
+
+from app.store.database.gino import db
+
+
+@dataclass
+class Admin:
+    id: int
+    email: str
+    password: Optional[str] = None
+
+    def is_password_valid(self, password: str):
+        return self.password == sha256(password.encode()).hexdigest()
+
+    @classmethod
+    def from_session(cls, session: Optional[dict]) -> Optional["Admin"]:
+        return cls(id=session["admin"]["id"], email=session["admin"]["email"])
+
+
+class AdminModel(db.Model):
+    __tablename__ = "admins"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
+    def is_password_valid(self, password: str):
+        return self.password == sha256(password.encode()).hexdigest()
+
---
+++ app/store/admin/accessor.py
@@ -0,0 +1,27 @@
+import typing
+from hashlib import sha256
+from typing import Optional
+
+from app.base.base_accessor import BaseAccessor
+from app.admin.models import AdminModel
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
+
+
+class AdminAccessor(BaseAccessor):
+    async def connect(self, app: "Application"):
+        await super().connect(app)
+        await app.database.connect()
+        await self.create_admin(
+            email=app.config.admin.email, password=app.config.admin.password
+        )
+        await self.get_by_email(app.config.admin.email)
+
+    async def get_by_email(self, email: str) -> Optional[AdminModel]:
+        admin = await AdminModel.query.where(AdminModel.email == email).limit(1).gino.first()
+        return admin
+
+    async def create_admin(self, email: str, password: str) -> AdminModel:
+        admin = await AdminModel.create(email=email, password=sha256(password.encode()).hexdigest())
+        return admin
